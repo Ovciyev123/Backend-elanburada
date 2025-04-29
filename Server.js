cf74b214cc6cda1add6a226237aaffecd243c178
Ovciyev123 <faganio-af206@code.edu.az>
2025-04-30 00:37:57 +0400

    hdc12332555feq

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ import { Server } from "socket.io";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
   origin: 'https://frontend-elanburada.vercel.app',
