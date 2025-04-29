6ebe8aa08ce8d6ff3a1ad380e41ea3ed9671ddf2
Ovciyev123 <faganio-af206@code.edu.az>
2025-04-30 00:37:57 +0400

    hdc12332555feq

--- Middlewares/FileUpload.js
+++ Middlewares/FileUpload.js
@@ -1,15 +1,13 @@
-import multer from "multer"
+import multer from 'multer';
+import path from 'path';
 
-const storage=multer.diskStorage({
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'uploads')); // BURADA tam yol verilir
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  }
+});
 
-    destination:(req,file,cb)=>{
-
-        return cb(null,"./uploads")
-    },
-    filename:(req,file,cb)=>{
-
-        return cb(null,`${Date.now()}_${file.originalname}`)
-    }
-})
-
-export const upload=multer({storage})
+const upload = multer({ storage });
