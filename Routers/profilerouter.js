13ef23b85ed9089a78d1764aa362693b847ddaa0
Ovciyev123 <faganio-af206@code.edu.az>
2025-04-30 22:02:42 +0400

    333333333122bgb

--- Routers/profilerouter.js
+++ Routers/profilerouter.js
@@ -14,7 +14,7 @@ import upload from '../Middlewares/FileUpload.js';
 const profilerouter = express.Router();
 
 // Məlumatları email ilə əldə etmək üçün yeni route
-profilerouter.get('/profile/by-email/:email', getUserProfileByEmail); 
+profilerouter.get('/by-email/:email', getUserProfileByEmail); 
 profilerouter.post('/', upload.single('profileImage'), createUserProfile);
 profilerouter.get('/', getAllUserProfiles);
 profilerouter.get('/user/:id', getUserProfileById);
