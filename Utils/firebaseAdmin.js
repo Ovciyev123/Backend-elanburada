cf8ac17a1d2845e366f3de73489dbdbd29e78167
Ovciyev123 <faganio-af206@code.edu.az>
2025-05-25 12:35:39 +0400

    dayi1fefewfwef31

--- Utils/firebaseAdmin.js
+++ Utils/firebaseAdmin.js
@@ -1,8 +1,16 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../service-account.json';
+import serviceAccount from './serviceAccountKey.json' assert { type: "json" };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+try {
+  console.log('📦 Firebase Admin: Service account yükleniyor...');
+  
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  console.log('✅  Admin başarıyla başlatıldı.');
+} catch (error) {
+  console.error('❌ Firebase Admin :', error);
+}
 
 export default admin;
