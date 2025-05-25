c0e99ad5330ad3dbfb3e5667d6bf1f3eb654fc42
Ovciyev123 <faganio-af206@code.edu.az>
2025-05-25 14:00:36 +0400

    dayi1fefewfwef31

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,18 @@
 import admin from "firebase-admin";
-import fs from "fs";
 
-// JSON dosyasını oku ve parse et
+// Ortam değişkeninden base64 string alınır
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+if (!serviceAccountBase64) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_BASE64 environment variable not set.");
+}
+
+// Base64 string'i çöz ve JSON'a çevir
 const serviceAccount = JSON.parse(
-  fs.readFileSync("./config/serviceAccountKey.json", "utf8")
+  Buffer.from(serviceAccountBase64, "base64").toString("utf-8")
 );
 
+// Admin SDK'yı başlat
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
